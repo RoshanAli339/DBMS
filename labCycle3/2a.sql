81278b21dd4b85dcc4855c7635f706bab890f2a2
Roshan Ali <69452049+RoshanAli339@users.noreply.github.com>
2023-06-24 09:35:26 +0530

    Update 2a.sql

---
+++ labCycle3/2a.sql
@@ -0,0 +1,33 @@
+declare
+    n1 DEMO_TAB.Num1%type := &n1;
+    n2 DEMO_TAB.Num2%type := &n2;
+    ma number;
+    mi number;
+    gcd DEMO_TAB.HCF%type;
+    lcm DEMO_TAB.LCM%type;
+    i number;
+    r DEMO_TAB%ROWTYPE;
+begin
+    if n1>n2 then
+    ma := n1;
+    mi := n2;
+    else
+    mi := n1;
+    ma := n2;
+    end if;
+    for i in 2..mi loop
+    if MOD(mi, i)=0 AND MOD(ma, i)=0 then   
+    lcm := i;
+    EXIT;
+    end if;
+    end loop;
+
+    for i in REVERSE 1..mi loop
+    if MOD(mi, i)=0 AND MOD(ma, i)=0 then
+    gcd := i;
+    EXIT;
+    end if;
+    end loop;
+
+    INSERT INTO DEMO_TAB VALUES(n1, n2, lcm, gcd);
+end;
