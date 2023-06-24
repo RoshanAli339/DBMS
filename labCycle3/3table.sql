8d5842de0254ca2690b37e7375293e0c477a8644
Roshan Ali <69452049+RoshanAli339@users.noreply.github.com>
2023-06-24 09:51:15 +0530

    Add files via upload

---
+++ labCycle3/3table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Inventory(
+    Product_ID      numeric(5,0),
+    Product_name    varchar(20),
+    Quntity         number(3,0),
+    PRIMARY KEY(Product_ID)
+);
+
+CREATE TABLE Purchase_Record(
+    Product_ID      numeric(5,0),
+    Status          varchar(20),
+    TransacDate            date,
+    FOREIGN KEY (Product_ID) REFERENCES Inventory
+);
